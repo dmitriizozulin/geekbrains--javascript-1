3d78015869dbad3c466b49cf7d559241f12837c6
Dmitrii Zozulin <dmitriizozulin@gmail.com>
2020-07-11 21:18:21 +0300

    homework-1-2

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 // Tf = (9 / 5) * Tc + 32
 const t_celsius = 30;
 const t_fahrenheit = (9 / 5) * t_celsius + 32;
-// alert(t_fahrenheit);
+alert(t_fahrenheit);
 
 const name = 'Василий';
 const admin = name;
-// alert(admin);
+console.log(admin);
 
 let result = 10 + 10 + "10";
 console.log(result);
